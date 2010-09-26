c755ff0aa4ce6955df14786b015a6386f58f792c
Oleg Slobodskoi <oleg008@gmail.com>
2010-09-27 00:00:57 +0200

    added boolean detection

--- test/test.js
+++ test/test.js
@@ -4,37 +4,31 @@ var a = require( "assert" ),
     parse = require( "./../lib/argsparser" ).parse,
     sys = require( "sys" );
 
-sys.print( "Run argsparser tests...\n" );
+sys.print( "Run tests...\n" );
 
-// node script.js
-a.deepEqual( parse(), {node: __filename} );
+a.deepEqual( parse(), {node: __filename}, "node script.js" );
 
-// node script.js -o
-a.deepEqual( parse(["node", __filename, "-o"]), {node: __filename, "-o": true} );
+a.deepEqual( parse(["node", __filename, "-o"]), {node: __filename, "-o": true}, "node script.js -o" );
 
-// node script.js -o test
-a.deepEqual( parse(["node", __filename, "-o", "test"]), {node: __filename, "-o": "test"} );
+a.deepEqual( parse(["node", __filename, "-o", "true"]), {node: __filename, "-o": true}, "node script.js -o true" );
 
-// node script.js -a testa -b testb
-a.deepEqual( parse(["node", __filename, "-a", "testa", "-b", "testb"]), {node: __filename, "-a": "testa", "-b": "testb"} );
+a.deepEqual( parse(["node", __filename, "-o", "false"]), {node: __filename, "-o": false}, "node script.js -o false" );
 
-// node script.js --a testa --b testb 
-a.deepEqual( parse(["node", __filename, "--a", "testa", "--b", "testb"]), {node: __filename, "--a": "testa", "--b": "testb"} );
+a.deepEqual( parse(["node", __filename, "-o", "test"]), {node: __filename, "-o": "test"}, "node script.js -o test" );
 
-// node script.js -a testa --b testb 
-a.deepEqual( parse(["node", __filename, "-a", "testa", "--b", "testb"]), {node: __filename, "-a": "testa", "--b": "testb"} );
+a.deepEqual( parse(["node", __filename, "-a", "testa", "-b", "testb"]), {node: __filename, "-a": "testa", "-b": "testb"}, "node script.js -a testa -b testb" );
 
-// node script.js --a testa -b testb 
-a.deepEqual( parse(["node", __filename, "--a", "testa", "-b", "testb"]), {node: __filename, "--a": "testa", "-b": "testb"} );
+a.deepEqual( parse(["node", __filename, "--a", "testa", "--b", "testb"]), {node: __filename, "--a": "testa", "--b": "testb"}, "node script.js --a testa --b testb " );
 
-// node script.js -paths /test.js /test1.js
-a.deepEqual( parse(["node", __filename, "-paths", "/test.js", "/test1.js"]), {node: __filename, "-paths": ["/test.js", "/test1.js"]} );
+a.deepEqual( parse(["node", __filename, "-a", "testa", "--b", "testb"]), {node: __filename, "-a": "testa", "--b": "testb"}, "node script.js -a testa --b testb" );
 
-// node script.js --paths /test.js /test1.js
-a.deepEqual( parse(["node", __filename, "--paths", "/test.js", "/test1.js"]), {node: __filename, "--paths": ["/test.js", "/test1.js"]} );
+a.deepEqual( parse(["node", __filename, "--a", "testa", "-b", "testb"]), {node: __filename, "--a": "testa", "-b": "testb"}, "node script.js --a testa -b testb" );
 
-// node script.js --paths /test.js /test1.js -a testa
-a.deepEqual( parse(["node", __filename, "--paths", "/test.js", "/test1.js", "-a", "testa"]), {node: __filename, "--paths": ["/test.js", "/test1.js"], "-a": "testa"} );
+a.deepEqual( parse(["node", __filename, "-paths", "/test.js", "/test1.js"]), {node: __filename, "-paths": ["/test.js", "/test1.js"]}, "node script.js -paths /test.js /test1.js" );
+
+a.deepEqual( parse(["node", __filename, "--paths", "/test.js", "/test1.js"]), {node: __filename, "--paths": ["/test.js", "/test1.js"]}, "node script.js --paths /test.js /test1.js" );
+
+a.deepEqual( parse(["node", __filename, "--paths", "/test.js", "/test1.js", "-a", "testa"]), {node: __filename, "--paths": ["/test.js", "/test1.js"], "-a": "testa"}, "node script.js --paths /test.js /test1.js -a testa" );
 
 
 sys.print( "All tests ok\n" );
